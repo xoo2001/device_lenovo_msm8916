288c132064865e8e0172c0153765126535e9fa02
Jaroslav Zviezda <acroreiser@gmail.com>
2020-09-12 14:22:22 +0300

    a6000: bring up RR Q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,8 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/lineage_a6000.mk \
+    $(LOCAL_DIR)/rr_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    lineage_a6000-userdebug \
+    rr_a6000-userdebug
