0a36062fda658b9024ce5d80f778630732b01e86
xoo2001 <ekora2001@gmail.com>
2021-05-21 06:23:01 +0000

    a6000: tree Nad

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/nad_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    nad_a6000-userdebug
