d12342c1e631bca519517519df2244dc0e680650
xoo2001 <ekora2001@gmail.com>
2021-06-06 15:02:10 +0700

    a6000: Havoc-Ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,6 +1,6 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a6000.mk
+    $(LOCAL_DIR)/havoc_a6000.mk
 
 # Lunch choices
 COMMON_LUNCH_CHOICES := \
-    lineage_a6000-userdebug
+    havoc_a6000-userdebug
--- havoc_a6000.mk
+++ havoc_a6000.mk
@@ -16,7 +16,7 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/lenovo/a6000/device.mk)
-$(call inherit-product, vendor/lineage/config/common_mini_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8916
@@ -26,7 +26,7 @@ TARGET_OTA_ASSERT_DEVICE := Kraft-T,a6000,K30-T,A6000,Kraft-W,Kraft-C,k30t,msm89
 
 TARGET_VENDOR := Lenovo
 PRODUCT_DEVICE := a6000
-PRODUCT_NAME := lineage_a6000
+PRODUCT_NAME := havoc_a6000
 BOARD_VENDOR := Lenovo
 PRODUCT_BRAND := Lenovo
 PRODUCT_MODEL := A6000
